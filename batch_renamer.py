92d9818c5bd5b43fa8b41ff770581cd1d69ee4c9
Matt <mchalc1@wgu.edu>
2023-07-26 15:51:47 -0700

    - Cleanup and add comments

--- batch_renamer.py
+++ batch_renamer.py
@@ -1,10 +1,11 @@
 import argparse, glob, os, sys, re
 
+# argparse setup
 parser = argparse.ArgumentParser(prog="batch_renamer.py",
                                  description='Batch rename files in folder.')
 parser.add_argument('-d', '--directory',
-                    help=r'[Optional: default=current directory]\n' +
-                    'Full path to directory (e.g. "c:\temp")',
+                    help='[Optional: default=current directory]\n' +
+                    'Full path to directory in quotes. (e.g. "c:\temp")',
                     default=os.getcwd())
 parser.add_argument('-s', '--select',
                     help="Pattern selection to determine what files to " +
@@ -13,7 +14,7 @@ parser.add_argument('-s', '--select',
 parser.add_argument('-i', '--increment',
                     help="Add a number that will increment after each file " +
                          "is renamed. This will overwrite characters to the" +
-                         " left in the old string if the incrementor " +
+                         " left in the old string when the incrementor " +
                          "increases in digits (e.g. 138)",
                     type=int, default=None)
 parser.add_argument('-m', '--multiplier',
@@ -29,22 +30,23 @@ parser.add_argument('-n', '--name', nargs='*',
                     required=True)
 parser.add_argument('--test', action='store_const', const=True, default=None,
                     help="Runs the program in test mode. Does not rename any " +
-                         "files. Only prints test filenames to console.")
+                         "files. Only prints renamed files to console.")
 args = parser.parse_args()
 
+
 def main(args):
+    # Test flag conditional
     if args.test is not None:
         test_rename(args.directory, args.select, args.name, args.increment, args.multiplier)
     else:
         rename(args.directory, args.select, args.name, args.increment, args.multiplier)
 
 
-'''
-USAGE:
-batch_renamer.py -d "c:/temp" -s *.doc -i 138 -m 2 -n "this " "%d " "is " "%s " "new"
-
-The above example will convert all *.doc files in c:/temp dir to new%d%s.doc,
-where %d is the incrementor and %s is the previous base name of the file.
+''' function to rename file names
+- break up the filepath in to directory(dir), filename(title), and extention(ext)
+- if increment argument is given, then the title and ext is passed to
+    name_recombobulator(). The modified filename is returned.
+- if the is no increment argument given then the file is renamed simply and returned
 '''
 def rename(dir, pattern, titlePattern, incrementor, multiplier):
     initIncrementorLength = len(str(incrementor))
@@ -56,13 +58,17 @@ def rename(dir, pattern, titlePattern, incrementor, multiplier):
                                               int(fileCount // multiplier + incrementor),
                                               title, ext,
                                               initIncrementorLength)
+            
+            # try except block catches if the filename already exists
             try:
                 os.rename(pathAndFilename, newFilename)
             except FileExistsError:
-                print(f"ERROR: The file name '{newFilename}' already exists and cannot be created.")
+                print(f"ERROR: The file name '{newFilename}' already exists " +
+                      "and cannot be created.")
                 isBadInput = True
                 while isBadInput:
-                    userInput = input("Do you wish to continue renaming? Y or N\n").upper()
+                    userInput = input("Do you wish to continue renaming? " +
+                                      "Y or N\n").upper()
                     if userInput == 'Y':
                         isBadInput = False
                         continue
@@ -72,14 +78,16 @@ def rename(dir, pattern, titlePattern, incrementor, multiplier):
                     else:
                         print("Please type 'Y' or 'N'.")
                         continue
-            
-
+            # increment the fileCount to do 'multiplier' math
             fileCount += 1
+
         else:
             titlePattern = "".join(titlePattern)
             os.rename(pathAndFilename,
                   os.path.join(dir, titlePattern % title + ext))
 
+
+# everything that the Rename() function does, but outputs to console instead.
 def test_rename(dir, pattern, titlePattern, incrementor, multiplier):
     initIncrementorLength = len(str(incrementor))
     fileCount = 0
@@ -96,53 +104,50 @@ def test_rename(dir, pattern, titlePattern, incrementor, multiplier):
             titlePattern = "".join(titlePattern)
             print(os.path.join(dir, titlePattern % title + ext))
 
+
 '''
-Take the pattern and remove the length of the incrementor plus any newly
-renamed characters. That length is removed from the front of the original
-title. The new filename path is then constructed out of the new pattern
-with the correct characters substituted.
+- Take the pattern and remove the length of the incrementor plus any newly
+    renamed characters.
+- That length is removed from the front of the original title.
+- The new filename path is then constructed out of the new pattern with the
+    correct characters substituted.
 '''
 def name_recombobulator(dir, titlePattern, incrementor, title, ext,
                initIncrementorLength):
-    '''if the incrementor increases in  magnitude the amount of digits will
-    increase. We need to slice the titlePattern if that happens, but only from
-    the prefix (e.g. "new%d%s" splits to prefix="new", suffix="%d%s")'''
+    ''' if the incrementor increases in digits we need to slice the
+    titlePattern if that happens, but only from the previous argument
+    (e.g. "new%d%s" splits to prefix="new", suffix="%d%s") '''
+    # these two list comps gets the index of the incrementor and the old
+    # filename arguments
     incrementorIndex = [titlePattern.index(x) for x in titlePattern if re.match("%d", x)][0]
     oldTitleIndex = [titlePattern.index(x) for x in titlePattern if re.match("%s", x)][0]
 
     incrementorLengthDelta = len(str(incrementor)) - initIncrementorLength
-    #modifiedPattern = re.sub(r"%.", "", titlePattern)
 
     '''modifiy the argument before the incrementor argument if the incrementor
     increase digits '''
     if (incrementorLengthDelta > 0):
-        #patternPrefix = modifiedPattern
         modifiedPrefix = titlePattern[incrementorIndex - 1]
-        #patternSuffix = titlePattern[len(modifiedPattern):len(titlePattern)]
-        #modifiedPrefix = patternPrefix[0:incrementorLengthDelta * -1]
         modifiedPrefix = modifiedPrefix[0:incrementorLengthDelta * -1]
-        #modifiedTitlePattern = modifiedPrefix + patternSuffix
         titlePattern[incrementorIndex - 1] = modifiedPrefix
-
-    #replaceLength = len(modifiedPattern) + len(str(incrementor))
-    #modifiedTitle = title[replaceLength:]
-    #combinedStr = titlePattern % (incrementor, modifiedTitle + ext)
+    
+    # insert the incrementor into the "%d" argument
     titlePattern[incrementorIndex] = titlePattern[incrementorIndex] % incrementor
+    
+    # get the length of the incrementor plus length of all characters to the
+    # left of the incrementor.
     modifiedTitle = ""
     for index in range(0, incrementorIndex + 1):
         modifiedTitle += titlePattern[index]
     modifiedTitle = title[len(modifiedTitle):]
+    
+    # insert a slice of the old file name
     titlePattern[oldTitleIndex] = titlePattern[oldTitleIndex] % modifiedTitle
+
+    # join all the modified arguments and the extension
     combinedStr = "".join(titlePattern) + ext
     return os.path.join(dir, combinedStr)
 
-
-'''
-take any number of arguments in the renaming pattern (e.g. command:
-batchrenamer.py -d c:\temp -s s01e00*.mp4 -i 1
-                -n "this - " "%d " "is " "%s " "- new.ext")
-
-'''
 
 if __name__ == "__main__":
     main(args)
